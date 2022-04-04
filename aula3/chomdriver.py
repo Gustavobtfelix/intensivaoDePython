e507a4f325e14fab9ab31fd194c58cfa3c04e017
Gustavo <gustavofelix01@yahoo.com>
2022-04-04 20:35:30 -0300

    Adding Path
    Webdriver Path

--- aula3/chomdriver.py
+++ aula3/chomdriver.py
@@ -2,5 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(executable_path='C://Users/guste/Downloads/chromedriver.exe')
+#driver = webdriver.Chrome(executable_path='C://Users/guste/Downloads/chromedriver.exe')
+driver = webdriver.Chrome(executable_path='C://Users/guste/Documents/GitHub/intensivaoDePython/Aula3/chromedriver.exe')
 driver.get("https://www.google.com.br")
+driver.quit()
